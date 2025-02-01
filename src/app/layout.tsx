dd7ff406797d256ab7b7d52f6cf85e46e75646c1
DanSias <dan.sias@gmail.com>
2025-02-01 13:03:46 -0500

    Added background images and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Image from "next/image";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +33,30 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50 dark:bg-slate-900`}>
         <ThemeProvider attribute="class" defaultTheme="system">
           <Header />
+
+          {/* Background Blur Elements */}
+          <Image
+            alt=""
+            width="530"
+            height="530"
+            decoding="async"
+            data-nimg="1"
+            className="hidden dark:xl:block absolute left-[-50px] md:left-8 top-4 opacity-50 blur-2xl"
+            src="/blur-cyan.png"
+            title=""
+          />
+          <Image
+            alt=""
+            width="567"
+            height="567"
+            decoding="async"
+            data-nimg="1"
+            className="hidden dark:xl:block absolute right-[-40px] md:right-8 top-24 opacity-50 blur-2xl"
+            src="/blur-indigo.png"
+            title=""
+          />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
