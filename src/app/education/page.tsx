99d3497b73b2f8354c32ea6e78fce2e9e530bffa
DanSias <dan.sias@gmail.com>
2025-02-01 12:56:47 -0500

    Updated Layout

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,17 +1,17 @@
-import EducationCard from "@/components/EducationCard";
+import EducationCard from "./DegreeDetails";
 import degrees from "@/constants/education";
 
 export default function EducationPage() {
   return (
     <main className="flex flex-col items-center justify-center pt-36 px-4">
       <div className="max-w-4xl w-full">
-        <h1 className="text-4xl font-bold text-center">Education</h1>
-        <p className="text-lg text-gray-600 dark:text-sky-200 text-center mt-2">
+        <h1 className="text-heading">Education</h1>
+        <p className="text-subhead">
           A summary of my academic background and key takeaways from each
           program.
         </p>
 
-        <div className="mt-6 flex flex-col gap-6">
+        <div className="mt-4 flex flex-col gap-6">
           {degrees.map((degree, index) => (
             <EducationCard key={index} degree={degree} />
           ))}
