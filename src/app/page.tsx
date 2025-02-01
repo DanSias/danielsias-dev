37d68d01366585d90bcc7e548233a1d4a2e570ae
DanSias <dan.sias@gmail.com>
2025-02-01 13:03:56 -0500

    Updated page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen pt-32">
       <HomePage />
     </div>
   );
